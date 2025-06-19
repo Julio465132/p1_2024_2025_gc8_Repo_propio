e22738e57b6c694972c9072ac7b1e6786d63106f
burru <burru@LAPTOP-6S6ME6G9>
2025-06-19 19:00:34 +0200

    Rutas definidas para json, json agrupado y csv

--- src/main/java/org/dis/Practica1/Main.java
+++ src/main/java/org/dis/Practica1/Main.java
@@ -1,4 +1,9 @@
 package org.dis.Practica1;
 
 public class Main {
+    public static String rutaCsv = "src/main/resources/TurismoComunidades.csv";
+    public static String rutaJson = "src/main/resources/TurismoComunidades.json";
+    public static String rutaJsonAgrupado = "src/main/resources/Comunidades_agrupadas.json";
+
+
 }
