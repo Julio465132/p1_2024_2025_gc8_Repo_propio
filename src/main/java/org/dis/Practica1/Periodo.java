677a8cf259929d8c1384ae6249927b6a683e3ac3
Borja2004 <9205627@alumnos.ufv.es>
2025-06-19 18:20:56 +0200

    Constructores añadidos

--- src/main/java/org/dis/Practica1/Periodo.java
+++ src/main/java/org/dis/Practica1/Periodo.java
@@ -4,4 +4,41 @@ public class Periodo {
     private String inicioPeriodo;
     private String finPeriodo;
     private String codigoPeriodo;
+
+    // Constructor para inicializar los campos
+    public Periodo(String inicioPeriodo, String finPeriodo, String codigoPeriodo) {
+        this.inicioPeriodo = inicioPeriodo;
+        this.finPeriodo = finPeriodo;
+        this.codigoPeriodo = codigoPeriodo;
+    }
+
+    // Getter de la fecha de inicio
+    public String getInicioPeriodo() {
+        return inicioPeriodo;
+    }
+
+    // Setter de la fecha de inicio
+    public void setInicioPeriodo(String inicioPeriodo) {
+        this.inicioPeriodo = inicioPeriodo;
+    }
+
+    // Getter de la fecha de fin
+    public String getFinPeriodo() {
+        return finPeriodo;
+    }
+
+    // Setter de la fecha de fin
+    public void setFinPeriodo(String finPeriodo) {
+        this.finPeriodo = finPeriodo;
+    }
+
+    // Getter del código del periodo
+    public String getCodigoPeriodo() {
+        return codigoPeriodo;
+    }
+
+    // Setter del código del periodo
+    public void setCodigoPeriodo(String codigoPeriodo) {
+        this.codigoPeriodo = codigoPeriodo;
+    }
 }
