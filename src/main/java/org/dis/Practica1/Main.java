1b7ec4f274134703d0f0453ff83ccbda93f0f992
burru <burru@LAPTOP-6S6ME6G9>
2025-06-19 19:02:37 +0200

    variables controladoras del teclado definididas

--- src/main/java/org/dis/Practica1/Main.java
+++ src/main/java/org/dis/Practica1/Main.java
@@ -1,9 +1,17 @@
 package org.dis.Practica1;
 
+import java.util.Scanner;
+
 public class Main {
     public static String rutaCsv = "src/main/resources/TurismoComunidades.csv";
     public static String rutaJson = "src/main/resources/TurismoComunidades.json";
     public static String rutaJsonAgrupado = "src/main/resources/Comunidades_agrupadas.json";
 
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        boolean csvCargando = false;
+        boolean seguirEjecutando = true;
 
+
+    }
 }
