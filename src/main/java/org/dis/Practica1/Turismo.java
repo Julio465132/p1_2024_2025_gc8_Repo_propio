4b39a9d7af9d531af0abe448fa314271f97bf3ba
Borja2004 <9205627@alumnos.ufv.es>
2025-06-19 18:24:21 +0200

    Variables de la clase turismo

--- src/main/java/org/dis/Practica1/Turismo.java
+++ src/main/java/org/dis/Practica1/Turismo.java
@@ -1,4 +1,8 @@
 package org.dis.Practica1;
 
 public class Turismo {
+    private Origen origenViaje;
+    private Destino destinoViaje;
+    private Periodo periodoDatos;
+    private int numeroTuristas;
 }
