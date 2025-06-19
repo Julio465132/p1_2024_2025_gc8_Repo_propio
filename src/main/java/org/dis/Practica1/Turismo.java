ab9b62ff81f44da26c0552454444eae4cbec9783
Borja2004 <9205627@alumnos.ufv.es>
2025-06-19 18:24:52 +0200

    Constructores de la clase turismo

--- src/main/java/org/dis/Practica1/Turismo.java
+++ src/main/java/org/dis/Practica1/Turismo.java
@@ -5,4 +5,45 @@ public class Turismo {
     private Destino destinoViaje;
     private Periodo periodoDatos;
     private int numeroTuristas;
+    // Constructor principal
+    public Turismo(Origen origenViaje, Destino destinoViaje, Periodo periodoDatos, int numeroTuristas) {
+        this.origenViaje = origenViaje;
+        this.destinoViaje = destinoViaje;
+        this.periodoDatos = periodoDatos;
+        this.numeroTuristas = numeroTuristas;
+    }
+
+    // Getters y setters
+    public Origen getOrigenViaje() {
+        return origenViaje;
+    }
+
+    public void setOrigenViaje(Origen origenViaje) {
+        this.origenViaje = origenViaje;
+    }
+
+    public Destino getDestinoViaje() {
+        return destinoViaje;
+    }
+
+    public void setDestinoViaje(Destino destinoViaje) {
+        this.destinoViaje = destinoViaje;
+    }
+
+    public Periodo getPeriodoDatos() {
+        return periodoDatos;
+    }
+
+    public void setPeriodoDatos(Periodo periodoDatos) {
+        this.periodoDatos = periodoDatos;
+    }
+
+    public int getNumeroTuristas() {
+        return numeroTuristas;
+    }
+
+    public void setNumeroTuristas(int numeroTuristas) {
+        this.numeroTuristas = numeroTuristas;
+    }
+
 }
