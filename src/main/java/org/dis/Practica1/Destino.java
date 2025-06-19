40e3dd127429f782b5a523316e1ee707f6a6c26d
ovifernandez <davidenriquea@icloud.com>
2025-06-19 17:07:24 +0200

    Inicializo variables

--- src/main/java/org/dis/Practica1/Destino.java
+++ src/main/java/org/dis/Practica1/Destino.java
@@ -1,4 +1,12 @@
 package org.dis.Practica1;
 
 public class Destino {
+    private String regionDestino;
+    private String zonaDestino;
+
+    // Constructor con los datos necesarios
+    public Destino(String regionDestino, String zonaDestino) {
+        this.regionDestino = regionDestino;
+        this.zonaDestino = zonaDestino;
+    }
 }
