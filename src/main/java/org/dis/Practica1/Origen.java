8f13ecbe45374959debc2014e581d0c88dc5f7fe
Borja2004 <9205627@alumnos.ufv.es>
2025-06-19 18:14:52 +0200

    Contructores getter y setter

--- src/main/java/org/dis/Practica1/Origen.java
+++ src/main/java/org/dis/Practica1/Origen.java
@@ -3,4 +3,29 @@ package org.dis.Practica1;
 public class Origen {
     private String provinciaSalida;
     private String comunidadSalida;
+    // Constructor para inicializar los datos del origen
+    public Origen(String provinciaSalida, String comunidadSalida) {
+        this.provinciaSalida = provinciaSalida;
+        this.comunidadSalida = comunidadSalida;
+    }
+
+    // Getter de provincia
+    public String getProvinciaSalida() {
+        return provinciaSalida;
+    }
+
+    // Setter de provincia
+    public void setProvinciaSalida(String provinciaSalida) {
+        this.provinciaSalida = provinciaSalida;
+    }
+
+    // Getter de comunidad
+    public String getComunidadSalida() {
+        return comunidadSalida;
+    }
+
+    // Setter de comunidad
+    public void setComunidadSalida(String comunidadSalida) {
+        this.comunidadSalida = comunidadSalida;
+    }
 }
